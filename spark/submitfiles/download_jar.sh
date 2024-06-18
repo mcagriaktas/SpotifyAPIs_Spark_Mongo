41e1c7b4a4eef4d1ce93a1eb8e11ebdad111c0d8
Cagri <52080028+mcagriaktas@users.noreply.github.com>
2024-06-18 23:23:49 +0300

    Delete spark directory

--- spark/submitfiles/download_jar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-URL=$1
-
-docker exec -it spark-master bash -c "cd /opt/spark/jars && wget '$URL'"
-
-echo "Your jar file has been downloaded in the spark-master container at /opt/spark/jars/"
-
--- spark/submitfiles/spark-submit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker exec spark-master spark-submit --master spark://172.80.0.105:7077 /opt/submitfiles/"$@"
